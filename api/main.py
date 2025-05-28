f1a13129a4365f85b654e58839ce4b2d6b7bbaf3
Otmane Boumediane <boumediane.othmane@gmail.com>
2025-05-28 22:24:19 +0100

    minor changes

--- api/main.py
+++ api/main.py
@@ -38,5 +38,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="localhost", port=8000)
